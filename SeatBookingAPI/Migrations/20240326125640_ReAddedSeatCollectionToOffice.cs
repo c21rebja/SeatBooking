12b02394cd76c2efd06b81c9a40cf36766e48f45
Rebecka Jansson <rebecka.jansson@addgile.com>
2024-03-26 14:00:40 +0100

    Re-added collection of seats to office

---
+++ SeatBookingAPI/Migrations/20240326125640_ReAddedSeatCollectionToOffice.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeatBookingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReAddedSeatCollectionToOffice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Seat_OfficeId",
+                table: "Seat",
+                column: "OfficeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seat_Office_OfficeId",
+                table: "Seat",
+                column: "OfficeId",
+                principalTable: "Office",
+                principalColumn: "OfficeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seat_Office_OfficeId",
+                table: "Seat");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Seat_OfficeId",
+                table: "Seat");
+        }
+    }
+}
